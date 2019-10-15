29b974a37036ff2e1fc38cedbca6ad09bddbce06
Breno Salles <breno-salles@hotmail.com>
2019-10-15 11:47:44 +0100

    Removed wrong classpath

--- src/RMIClient.java
+++ src/RMIClient.java
@@ -5,24 +5,6 @@ import java.rmi.RemoteException;
 
 public class RMIClient {
   public static void main(String[] args) {
-    Thread t = (new Thread() {
-      public void run() {
-        RMIRegistry.main(new String[0]);
-        RMIReplicaManager.main(new String[0]);
-        RMIServer.main(new String[] {"2025"});
-        RMIServer.main(new String[] {"2026"});
-        RMIServer.main(new String[] {"2027"});
-      }
-    });
-    t.start();
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    } // garante que todos os serviços estão disponíveis antes de executar o
-      // código do cliente
-
     PlacesListManagerInterface p1 = null;
     ObjectRegistryInterface p2 = null;
     PlacesListManagerInterface p3 = null;
@@ -32,15 +14,15 @@ public class RMIClient {
       System.out.println("Adicionar place no ReplicaManager");
       p1 = (PlacesListManagerInterface) Naming.lookup("rmi://localhost:2024/replicamanager");
       p1.addPlace(new Place("3500", "Viseu"));
-      
+
       System.out.println("Invocar um replica manager do object registry");
       p2 = (ObjectRegistryInterface) Naming.lookup("rmi://localhost:2023/registry");
       String url = p2.resolve("3500");
-      
+
       System.out.println("Invocar uma replica do replica manager");
       p3 = (PlacesListManagerInterface) Naming.lookup(url);
       String url2 = p3.getPlaceListAddress("3500");
-      
+
       System.out.println("Invocar o object place da replica");
       p4 = (PlacesListInterface) Naming.lookup(url2);
       System.out.println(p4.getPlace("3500").getLocality());
@@ -48,6 +30,6 @@ public class RMIClient {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    
+
   }
 }
